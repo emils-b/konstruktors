eeef2032461b295e44daf130f5ad89b3435d66bd
emils-b <emils.baskers@gmail.com>
2022-10-17 19:00:23 +0000

    Pievienoju komentarus naked.py

--- naked.py
+++ naked.py
@@ -19,31 +19,43 @@ dt = datetime.now()
 request_date = str(dt.year) + "-" + str(dt.month).zfill(2) + "-" + str(dt.day).zfill(2)  
 print("Generated today's date: " + str(request_date))
 
-
+# Request info from NASA API
 print("Request url: " + str(nasa_api_url + "rest/v1/feed?start_date=" + request_date + "&end_date=" + request_date + "&api_key=" + nasa_api_key))
 r = requests.get(nasa_api_url + "rest/v1/feed?start_date=" + request_date + "&end_date=" + request_date + "&api_key=" + nasa_api_key)
 
+# Printing NASA request response data
 print("Response status code: " + str(r.status_code))
 print("Response headers: " + str(r.headers))
 print("Response content: " + str(r.text))
 
+# Check if request got status code of successful response
 if r.status_code == 200:
 
+	# parses response json
 	json_data = json.loads(r.text)
 
 	ast_safe = []
 	ast_hazardous = []
 
+	# checks if response contains element_count key
 	if 'element_count' in json_data:
+		# adds asteroid count from response
 		ast_count = int(json_data['element_count'])
+		# prints asteroid count
 		print("Asteroid count today: " + str(ast_count))
 
+		# checks if asteroid count is more than 0
 		if ast_count > 0:
+			# iterates through each near earth objects value in response data for todays date 
 			for val in json_data['near_earth_objects'][request_date]:
+				# checks if val contains further used keys
 				if 'name' and 'nasa_jpl_url' and 'estimated_diameter' and 'is_potentially_hazardous_asteroid' and 'close_approach_data' in val:
+					# stores asteroid name and url to use in this iteration
 					tmp_ast_name = val['name']
 					tmp_ast_nasa_jpl_url = val['nasa_jpl_url']
+					# checks if estimated diameter contains kilometers key
 					if 'kilometers' in val['estimated_diameter']:
+						# if estimated diameter min and max are given stores to use in this iteration otherwise saves as -2 value
 						if 'estimated_diameter_min' and 'estimated_diameter_max' in val['estimated_diameter']['kilometers']:
 							tmp_ast_diam_min = round(val['estimated_diameter']['kilometers']['estimated_diameter_min'], 3)
 							tmp_ast_diam_max = round(val['estimated_diameter']['kilometers']['estimated_diameter_max'], 3)
@@ -54,27 +66,35 @@ if r.status_code == 200:
 						tmp_ast_diam_min = -1
 						tmp_ast_diam_max = -1
 
+					# stores value if asterioid is hazardous to use in this iteration
 					tmp_ast_hazardous = val['is_potentially_hazardous_asteroid']
 
+					# checks if data is provaded
 					if len(val['close_approach_data']) > 0:
 						if 'epoch_date_close_approach' and 'relative_velocity' and 'miss_distance' in val['close_approach_data'][0]:
 							tmp_ast_close_appr_ts = int(val['close_approach_data'][0]['epoch_date_close_approach']/1000)
+							# converts from timestamp to time in UTC timezone
 							tmp_ast_close_appr_dt_utc = datetime.utcfromtimestamp(tmp_ast_close_appr_ts).strftime('%Y-%m-%d %H:%M:%S')
+							# converts time from timestamp
 							tmp_ast_close_appr_dt = datetime.fromtimestamp(tmp_ast_close_appr_ts).strftime('%Y-%m-%d %H:%M:%S')
 
+							# stores approach speed if given
 							if 'kilometers_per_hour' in val['close_approach_data'][0]['relative_velocity']:
 								tmp_ast_speed = int(float(val['close_approach_data'][0]['relative_velocity']['kilometers_per_hour']))
 							else:
 								tmp_ast_speed = -1
 
+							# stores distance if given
 							if 'kilometers' in val['close_approach_data'][0]['miss_distance']:
 								tmp_ast_miss_dist = round(float(val['close_approach_data'][0]['miss_distance']['kilometers']), 3)
 							else:
 								tmp_ast_miss_dist = -1
+						# populates variables if data is not given
 						else:
 							tmp_ast_close_appr_ts = -1
 							tmp_ast_close_appr_dt_utc = "1969-12-31 23:59:59"
 							tmp_ast_close_appr_dt = "1969-12-31 23:59:59"
+					# prints message and populates variable with standart data
 					else:
 						print("No close approach data in message")
 						tmp_ast_close_appr_ts = 0
@@ -83,6 +103,7 @@ if r.status_code == 200:
 						tmp_ast_speed = -1
 						tmp_ast_miss_dist = -1
 
+					# prints previously aggregated info
 					print("------------------------------------------------------- >>")
 					print("Asteroid name: " + str(tmp_ast_name) + " | INFO: " + str(tmp_ast_nasa_jpl_url) + " | Diameter: " + str(tmp_ast_diam_min) + " - " + str(tmp_ast_diam_max) + " km | Hazardous: " + str(tmp_ast_hazardous))
 					print("Close approach TS: " + str(tmp_ast_close_appr_ts) + " | Date/time UTC TZ: " + str(tmp_ast_close_appr_dt_utc) + " | Local TZ: " + str(tmp_ast_close_appr_dt))
@@ -97,20 +118,27 @@ if r.status_code == 200:
 		else:
 			print("No asteroids are going to hit earth today")
 
+	# prints amount of near flying and safe asteroids
 	print("Hazardous asteorids: " + str(len(ast_hazardous)) + " | Safe asteroids: " + str(len(ast_safe)))
 
+	# prints info about near flying asteroids gathered in the loop or message that there is none
 	if len(ast_hazardous) > 0:
 
+		# sorts hazardous asteroids based on time when they will fly by (starting earliest)
 		ast_hazardous.sort(key = lambda x: x[4], reverse=False)
 
+		# loops throught and prints info about each  asteroid
 		print("Today's possible apocalypse (asteroid impact on earth) times:")
 		for asteroid in ast_hazardous:
 			print(str(asteroid[6]) + " " + str(asteroid[0]) + " " + " | more info: " + str(asteroid[1]))
 
+		# sorts hazardous asteroids based on distance they will fly by (starting nearest)
 		ast_hazardous.sort(key = lambda x: x[8], reverse=False)
+		# loops throught and prints info about each asteroid
 		print("Closest passing distance is for: " + str(ast_hazardous[0][0]) + " at: " + str(int(ast_hazardous[0][8])) + " km | more info: " + str(ast_hazardous[0][1]))
 	else:
 		print("No asteroids close passing earth today")
 
+# prints info that could not get response from API
 else:
 	print("Unable to get response from API. Response code: " + str(r.status_code) + " | content: " + str(r.text))
